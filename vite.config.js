420ea7d287247c2a6087d66e0a3b88ae7348d094
vivekkurhade <vivek020704@gmail.com>
2025-04-21 13:04:25 +0530

    dep

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  build: {
+    rollupOptions: {
+      external: ['slick-carousel/slick/slick.css']
+    }
+  },
 })
